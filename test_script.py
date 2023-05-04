973a4103aac44f234363c218e65db786c42e780f
em-ert <emsterskate@outlook.com>
2023-05-04 19:11:35 -0400

    Final code from Pi

--- test_script.py
+++ test_script.py
@@ -1,9 +1,24 @@
 from time import sleep
+import RPi.GPIO as GPIO
+import time
 
+# Tester code to check if GPIO pins could be used in a python script run from Go
+# Used to make sure this was a viable solution before writing the whole menu out!
+# This is the LED blink code from class (CS121).
 def main():
-    for i in range(100):
-        print(i)
-        sleep(1)
+    PIN = 15
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(PIN, GPIO.OUT)
+
+    print("LED On")
+    GPIO.output(PIN, GPIO.HIGH)
+
+    # One second pause for LED on
+    time.sleep(1)
+    print("LED Off")
+    GPIO.output(PIN, GPIO.LOW)
 
 
 if __name__ == "__main__":
